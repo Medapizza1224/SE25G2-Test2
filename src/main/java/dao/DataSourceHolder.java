d08f39b542ea93c455a1256f9779bf1a73b911c4
mgn901 <mgn901+dev@gmail.com>
2026-01-23 17:42:21 +0900

    chore: DataSourceHolderを本番環境でも使えるようにする

--- src/main/java/dao/DataSourceHolder.java
+++ src/main/java/dao/DataSourceHolder.java
@@ -1,30 +1,51 @@
 package dao;
 
 import javax.sql.DataSource;
+
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import java.net.URL;
 
-public class DataSourceHolder { // publicを追加
-    private static HikariConfig _hikariConfig;
-    private static DataSource _dataSource;
+/**
+ * DAOで利用する{@link javax.sql.DataSource}を提供する。
+ * 
+ * このクラスのインスタンスをDAOの{@code private}フィールドに割り当てておくことで、DAOの各メソッドで接続を取得する処理を記述しやすくなる。
+ * 
+ * <pre>{@code
+ * class SampleDao {
+ *   // `DataSourceHolder.dataSource`をDAOのフィールドに割り当てておく。
+ *   private final DataSource dataSource;
+ *   public SampleDao() {
+ *     this.dataSourceHolder = new DataSourceHolder().dataSource;
+ *   }
+ *   public List&lt;SampleEntity&gt; getAll() {
+ *     Connection connection = null;
+ *     try {
+ *       // `dataSource`フィールドに割り当てた`DataSource`オブジェクトの`getConnection`メソッドを呼び出して`java.sql.Connection`型のオブジェクトを得る。
+ *       connection = this.dataSource.getConnection();
+ *       // 何らかの処理
+ *     } catch (SQLException exception) {
+ *       // 何らかの処理
+ *     }
+ *   }
+ * }
+ * }</pre>
+ */
+class DataSourceHolder {
+  private static HikariConfig _hikariConfig;
+  private static DataSource _dataSource;
 
-    public final DataSource dataSource;
+  public final DataSource dataSource;
 
-    public DataSourceHolder() {
-        if (DataSourceHolder._hikariConfig == null) {
-            // クラスパスから設定ファイルを取得
-            URL resource = this.getClass().getClassLoader().getResource("dataSource.properties");
-            if (resource == null) {
-                throw new RuntimeException("dataSource.propertiesが見つかりません。");
-            }
-            DataSourceHolder._hikariConfig = new HikariConfig(resource.getPath());
-        }
-
-        if (DataSourceHolder._dataSource == null) {
-            DataSourceHolder._dataSource = new HikariDataSource(DataSourceHolder._hikariConfig);
-        }
-
-        this.dataSource = DataSourceHolder._dataSource;
+  public DataSourceHolder() {
+    if (DataSourceHolder._hikariConfig == null) {
+      DataSourceHolder._hikariConfig = new HikariConfig(
+          this.getClass().getClassLoader().getResource("dataSource.properties").getPath());
     }
+
+    if (DataSourceHolder._dataSource == null) {
+      DataSourceHolder._dataSource = new HikariDataSource(DataSourceHolder._hikariConfig);
+    }
+
+    this.dataSource = DataSourceHolder._dataSource;
+  }
 }
